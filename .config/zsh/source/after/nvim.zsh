58ef2c89e1082b316b892860488a314c375a9358
Noah Friedman <noahfriedman2@gmail.com>
2024-06-04 11:19:18 -0400

    modified: .config/nvim/lazy-lock.json
    modified: .config/zsh/source/after/nvim.zsh
    
    deleted: .config/zsh/source/after/nvim_sync.zsh

--- .config/zsh/source/after/nvim.zsh
+++ .config/zsh/source/after/nvim.zsh
@@ -2,4 +2,25 @@
 if [ -x `command -v nvim` ]; then
         export EDITOR='nvim'
         alias vim='nvim'
+
+        if [[ -v NVIM ]]; then
+                # Keep the current directory in sync with Neovim's when in the Neovim terminal
+                function nvim_sync_recv {
+                        local tmp=$(mktemp)
+                        command nvim --server $NVIM --remote-expr "getcwd()" 1>/dev/null 2>$tmp
+                        cd `cat $tmp`
+                }
+                function nvim_sync_send {
+                        command nvim --server $NVIM --remote-send "<CMD>cd $PWD<CR>"
+                }
+                autoload -Uz add-zsh-hook
+                add-zsh-hook preexec nvim_sync_recv
+                add-zsh-hook chpwd nvim_sync_send
+
+                # Create an alias for 'nvim' that sends the file to the parent Neovim instance
+                function nvim {
+                        command nvim --server $NVIM --remote-send "<Cmd>tabnew<CR>"
+                        command nvim --server $NVIM --remote-send "<Cmd>lua vim.schedule_wrap(vim.cmd.e)('$@')<CR>"
+                }
+        fi
 fi
