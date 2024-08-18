736670e627cff06de1251f22e8296a359bfb580c
Noah Friedman <noahfriedman2@gmail.com>
2024-08-18 10:37:27 -0400

    modified: .config/nvim/after/plugin/keymap/copilot.lua
    modified: .config/nvim/lua/config/copilot/chat.lua

--- .config/nvim/after/plugin/keymap/copilot.lua
+++ .config/nvim/after/plugin/keymap/copilot.lua
@@ -4,15 +4,23 @@ for _, config in ipairs {
     rhs = "<Cmd>Copilot panel<CR>",
   },
   {
-    lhs = "h",
-    rhs = function()
-      require "CopilotChat.integrations.telescope".pick(require "CopilotChat.actions".help_actions())
-    end,
+    lhs = "c",
+    rhs = "<Cmd>CopilotChat<CR>",
   },
   {
     lhs = "p",
     rhs = function()
-      require "CopilotChat.integrations.telescope".pick(require "CopilotChat.actions".prompt_actions())
+      local t = require "CopilotChat.actions".prompt_actions() or { actions = {} }
+      t.prompt = "Copilot Chat"
+      t.actions["Explain diagnostic"] = {
+        prompt = 'Please explain the following diagnostic issue in file: "',
+        selection = require "CopilotChat.select".diagnostics,
+      }
+      t.actions["Fix diagnostic"] = t.actions["FixDiagnostic"]
+      t.actions["FixDiagnostic"] = nil
+      t.actions["Commit staged"] = t.actions["CommitStaged"]
+      t.actions["CommitStaged"] = nil
+      require "CopilotChat.integrations.telescope".pick(t)
     end,
   },
 } do
--- .config/nvim/lua/config/copilot/chat.lua
+++ .config/nvim/lua/config/copilot/chat.lua
@@ -1,7 +1,18 @@
 local M = {}
 
 function M.setup()
-  require "CopilotChat".setup {}
+  require "CopilotChat".setup {
+    auto_insert_mode = true,
+    window = {
+      layout = "float",
+    },
+    mappings = {
+      submit_prompt = {
+        normal = "<A-CR>",
+        insert = "<A-CR>",
+      }
+    }
+  }
 end
 
 return M
