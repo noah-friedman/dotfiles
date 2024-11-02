a3254f57a3385f50879c816b52c37e2a697fae86
Noah Friedman <noah.friedman@torontomu.ca>
2024-11-02 15:46:53 -0400

    modified: .config/nvim/lua/lsp/arduino.lua

--- .config/nvim/lua/lsp/arduino.lua
+++ .config/nvim/lua/lsp/arduino.lua
@@ -6,8 +6,14 @@ require "util.configure".lsp {
     cmd = {
       "arduino-language-server",
       "-cli-config",
-      -- Make a symlink to the arduino-cli config file in your home directory in order to maintain platform agnosticism
-      vim.fn.expand "~/.arduino.yaml"
+      -- Make a symlink to the arduino-cli config file in your home directory in order to maintain
+      -- platform agnosticism
+      vim.env.HOME .. "/.arduino.yaml",
+
+      "-format-conf-path",
+      -- Make a symlink to the .clang-format file in your home directory in order to maintain
+      -- platform agnosticism
+      vim.env.HOME .. "/Library/Preferences/clangd/.clang-format"
     },
   },
   -- Don't override capabilities (using `util.configure.lsp`), as this LSP doesn't support them
